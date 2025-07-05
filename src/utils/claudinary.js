29ff37ed36fca9a01dc3d2d315dad27e43997bc9
Anmol Goyal <anmolgoyal7817@gmai.com>
2025-07-05 17:44:52 +0530

    fix all register controller bugs

--- src/utils/claudinary.js
+++ src/utils/claudinary.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from 'cloudinary';
-import fs from fs;
+import fs from "fs";
 
 cloudinary.config({ 
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
@@ -17,7 +17,7 @@ cloudinary.config({
             resource_type: "auto"
         })
         // file has been uploaded successfull
-        //console.log("file is uploaded on cloudinary ", response.url);
+       // console.log("file is uploaded on cloudinary ", response.url);
         fs.unlinkSync(localFilePath)
         return response;
 
